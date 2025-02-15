549cce46eabf4f60b639c8954f769962ad973ad3
Marcelo Duarte <celo.duarte.2005@gmail.com>
2025-02-15 17:33:52 -0300

    Project created

---
+++ src/main/java/com/duartecelo/pje_converter/PjeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.duartecelo.pje_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjeConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjeConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duartecelo/pje_converter/PjeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duartecelo.pje_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjeConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
